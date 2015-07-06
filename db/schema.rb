97636666544c326e8bf297284eba0b9c02bdb028
Mohamed Hegazy <m.hegazy94@hotmail.com>
2015-07-06 13:13:55 +0000

    Add user and create one-to-many relationship with articles

--- db/schema.rb
+++ db/schema.rb
@@ -11,23 +11,31 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141203020035) do
+ActiveRecord::Schema.define(version: 20150706005635) do
 
   create_table "articles", force: true do |t|
-    t.string   "title"
+    t.string   "title",      limit: nil
     t.text     "text"
-    t.datetime "created_at"
-    t.datetime "updated_at"
+    t.datetime "created_at",             null: false
+    t.datetime "updated_at",             null: false
+    t.integer  "user_id"
   end
 
   create_table "comments", force: true do |t|
-    t.string   "commenter"
+    t.string   "commenter",  limit: nil
     t.text     "body"
     t.integer  "article_id"
-    t.datetime "created_at"
-    t.datetime "updated_at"
+    t.datetime "created_at",             null: false
+    t.datetime "updated_at",             null: false
   end
 
   add_index "comments", ["article_id"], name: "index_comments_on_article_id"
+
+  create_table "users", force: true do |t|
+    t.string   "username",   limit: nil
+    t.string   "email",      limit: nil
+    t.datetime "created_at",             null: false
+    t.datetime "updated_at",             null: false
+  end
 
 end
